87a3f4a5485bfc4d684962f54c5ee99029f98e8d
朽木不可雕也 <aszswaz@163.com>
2022-08-30 12:47:51 +0800

    Modify init.sh

--- init.sh
+++ init.sh
@@ -6,22 +6,39 @@ source ../shell/lib/index.sh
 
 log_info "init neovim..."
 
-# 当前文件夹映射到 nvim 的默认配置文件路径
-if [[ ! -e "$HOME/.config/nvim" ]]; then
-    ln -s "$PWD" "$HOME/.config/nvim"
+config_path="$HOME/.config/nvim"
+
+if [[ -e $config_path ]]; then
+    if [[ -L $config_path ]]; then
+        if [[ $(readlink "$config_path") != "$PWD" ]]; then
+            rm -rf "$config_path"
+            ln -s "$PWD" "$config_path"
+        fi
+    else
+        rm -rf "$config_path"
+        ln -s "$PWD" "$config_path"
+    fi
+else
+    ln -s "$PWD" "$config_path"
 fi
 
 # 添加 vim 包管理器当当前用户和 root 用户
 # packer，github：https://github.com/wbthomason/packer.nvim
 nvim_share="${XDG_DATA_HOME:-$HOME/.local/share}/nvim"
 packer_path="$nvim_share/site/pack/packer/start/packer.nvim"
-[[ ! -e "$packer_path" ]] && git clone --depth 1 https://github.com/wbthomason/packer.nvim "$packer_path"
+
+if [[ -e "$packer_path" ]]; then
+    env -C "$packer_path" git pull
+else
+    git clone https://github.com/wbthomason/packer.nvim "$packer_path"
+fi
+
 # vim-plug，github：https://github.com/junegunn/vim-plug
 vim_plug_path="$nvim_share/site/autoload/plug.vim"
-[[ ! -e "$vim_plug_path" ]] && curl --disable -fLo "$vim_plug_path" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -o "$vim_plug_path" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # 将 maven 配置应用到 root 账户
-sudo $SHELL -e -c "
+sudo /bin/sh -e -c "
 [[ ! -e '/root/.config' ]] && mkdir '/root/.config'
 [[ ! -e '/root/.local/share' ]] && mkdir -p '/root/.local/share'
 [[ ! -e '/root/.config/nvim' ]] && ln -s '$PWD' '/root/.config/nvim'
