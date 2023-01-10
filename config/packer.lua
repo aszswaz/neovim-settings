4947be3cd8bf8d6cc3bb732296988c75ef3317a7
朽木不可雕也 <aszswaz@163.com>
2023-01-10 20:59:30 +0800

    backup

--- config/packer.lua
+++ config/packer.lua
@@ -8,7 +8,7 @@ return require("packer").startup(function()
     use "wbthomason/packer.nvim"
     -- File manager.
     use {
-        "kyazdani42/nvim-tree.lua",
+        "aszswaz/nvim-tree.lua",
         requires = {
             "kyazdani42/nvim-web-devicons",
         },
