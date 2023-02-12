7aee6dc48d38034ef0a63b3185a1ab560cb8fda8
朽木不可雕也 <aszswaz@163.com>
2023-02-12 14:55:21 +0800

    简化 autocmd 配置

--- config/autocmd.lua
+++ config/autocmd.lua
@@ -2,39 +2,14 @@ local util = require "util"
 local theme = require "theme"
 local storage = require "util.storage"
 
+local autocmd = vim.cmd.autocmd
 local createAutocmd = vim.api.nvim_create_autocmd
 
-createAutocmd("FileType", {
-    desc = "Sets the indentation width for some file types.",
-    pattern = { "json", "html", "xml", "yaml", "svg", "sql" },
-    callback = function()
-        vim.o.tabstop = 2
-    end,
-})
-
-createAutocmd("FileType", {
-    desc = "Some file types enable line breaks.",
-    pattern = { "text", "desktop", "markdown" },
-    callback = function()
-        vim.o.wrap = true
-    end,
-})
-
-createAutocmd("FileType", {
-    desc = "Set formatting options for all file types.",
-    callback = function()
-        vim.o.formatoptions = "jcroql"
-        vim.o.conceallevel = 0
-    end,
-})
-
-createAutocmd("FileType", {
-    pattern = { "tags" },
-    desc = "Some documents cannot use spaces as tabs.",
-    callback = function()
-        vim.o.expandtab = false
-    end,
-})
+autocmd [[FileType json,html,xml,yaml,svg,sql :set tabstop=2]]
+autocmd [[FileType text,desktop,markdown      :set wrap]]
+autocmd [[FileType tags                       :set noexpandtab]]
+autocmd [[FileType help                       :set conceallevel=0]]
+autocmd [[FileType *                          :set formatoptions="jcroql"]]
 
 createAutocmd("ColorScheme", {
     desc = "Customize some highlight groups and save the theme changes.",
@@ -44,11 +19,11 @@ createAutocmd("ColorScheme", {
     end,
 })
 
-createAutocmd("VimEnter", { desc = "Restores the user's theme configuration.", callback = theme.init })
-createAutocmd("VimLeave", { desc = "Save user preferences configuration.", callback = storage.save })
 createAutocmd("BufRead", {
     callback = function()
         local buffer = vim.api.nvim_get_current_buf()
         vim.bo[buffer].modifiable = not vim.bo[buffer].readonly
     end,
 })
+createAutocmd("VimEnter", { desc = "Restores the user's theme configuration.", callback = theme.init })
+createAutocmd("VimLeave", { desc = "Save user preferences configuration.", callback = storage.save })
