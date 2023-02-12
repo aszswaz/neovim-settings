556f817a39add9178f6a75e05d28496131e6542e
朽木不可雕也 <aszswaz@163.com>
2023-02-12 14:10:41 +0800

    1. 添加用于测试的初始化脚本 2. 修改插件管理脚本的代码结构

--- plugin/manager.lua
+++ plugin/manager.lua
@@ -1,64 +1,81 @@
 local packer = require "packer"
 
 -- Use packer.vim to manage plugins.
-
 -- packer，github：https://github.com/wbthomason/packer.nvim
-packer.startup(function()
+local M = {}
+
+local PLUGINS = {
     -- Let packer manager self.
-    use "wbthomason/packer.nvim"
+    "wbthomason/packer.nvim",
+    --git plugin
+    "tpope/vim-fugitive",
+    -- debug plugin
+    "puremourning/vimspector",
+    -- fish plugin
+    "nickeb96/fish.vim",
+    -- Translation plugin
+    "voldikss/vim-translator",
+    -- A gui function for nvim-qt.
+    "equalsraf/neovim-gui-shim",
+    -- Indent level display plugin
+    "lukas-reineke/indent-blankline.nvim",
+    -- Bookmark plugin.
+    "MattesGroeger/vim-bookmarks",
+    -- Start screen plugin.
+    "mhinz/vim-startify",
+    -- gdb debugging plugin.
+    "sakhnik/nvim-gdb",
+    -- Automatic pairing plug-in, which can automatically add or delete paired symbols, such as "()", "[]", "{}".
+    "windwp/nvim-autopairs",
+    -- Project configuration management plugin.
+    "aszswaz/project.nvim",
+    -- Terminal plugin.
+    { "akinsho/toggleterm.nvim", tag = "*" },
+    -- Programming language helper plugins.
+    { "neoclide/coc.nvim", branch = "release" },
     -- File manager.
-    use {
+    {
         "nvim-tree/nvim-tree.lua",
         requires = {
             "kyazdani42/nvim-web-devicons",
         },
-    }
+    },
     -- Tab page plugin.
-    use {
+    {
         "romgrk/barbar.nvim",
         requires = { "kyazdani42/nvim-web-devicons" },
-    }
-    --git plugin
-    use "tpope/vim-fugitive"
-    -- vscode theme
-    use "Mofiqul/vscode.nvim"
-    -- OneDark theme
-    use "navarasu/onedark.nvim"
-    -- Bottom status bar plugin
-    use {
-        "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons" },
-    }
-    -- debug plugin
-    use "puremourning/vimspector"
-    -- tokyonight theme
-    use "folke/tokyonight.nvim"
-    -- Terminal plugin.
-    use { "akinsho/toggleterm.nvim", tag = "*" }
-    -- fish plugin
-    use "nickeb96/fish.vim"
+    },
     -- Fuzzy lookup plugin.
-    use {
+    {
         "nvim-telescope/telescope.nvim",
         tag = "*",
         requires = { "nvim-lua/plenary.nvim" },
-    }
-    -- Translation plugin
-    use "voldikss/vim-translator"
-    -- Programming language helper plugins.
-    use { "neoclide/coc.nvim", branch = "release", run = vim.cmd.CocUpdate }
-    -- A gui function for nvim-qt.
-    use "equalsraf/neovim-gui-shim"
-    -- Indent level display plugin
-    use "lukas-reineke/indent-blankline.nvim"
-    -- Bookmark plugin.
-    use "MattesGroeger/vim-bookmarks"
-    -- Start screen plugin.
-    use "mhinz/vim-startify"
-    -- gdb debugging plugin.
-    use "sakhnik/nvim-gdb"
-    -- Automatic pairing plug-in, which can automatically add or delete paired symbols, such as "()", "[]", "{}".
-    use "windwp/nvim-autopairs"
-    -- Project configuration management plugin.
-    use "aszswaz/project.nvim"
+    },
+}
+
+local THEMES = {
+    -- vscode theme
+    "Mofiqul/vscode.nvim",
+    -- OneDark theme
+    "navarasu/onedark.nvim",
+    -- tokyonight theme
+    "folke/tokyonight.nvim",
+    -- Bottom status bar plugin
+    {
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons" },
+    },
+}
+
+function M.extend(dest, src)
+    for _, iterm in pairs(src) do
+        table.insert(dest, iterm)
+    end
+end
+
+M.extend(PLUGINS, THEMES)
+packer.startup(function()
+    for _, iterm in pairs(PLUGINS) do
+        use(iterm)
+    end
 end)
---
+++ test/init.lua
@@ -0,0 +1,8 @@
+local PROJECT_DIR = vim.fn.expand("<script>:h:h")
+local runtimePaths = vim.api.nvim_list_runtime_paths()
+runtimePaths[1] = PROJECT_DIR
+vim.o.runtimepath = vim.fn.join(runtimePaths, ",")
+
+local runtime = vim.cmd.runtime
+runtime "config/init.lua"
+runtime "lua/init.lua"
