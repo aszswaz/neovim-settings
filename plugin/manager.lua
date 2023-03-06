e1d6eedfae843bc8410504d5ad855d291e8277f8
朽木不可雕也 <aszswaz@163.com>
2023-03-06 20:56:01 +0800

    backup

--- plugin/manager.lua
+++ plugin/manager.lua
@@ -1,4 +1,5 @@
 local packer = require "packer"
+local event = require "aszswaz.event"
 
 -- Use packer.vim to manage plugins.
 -- packer，github：https://github.com/wbthomason/packer.nvim
@@ -18,19 +19,87 @@ local PLUGINS = {
     -- A gui function for nvim-qt.
     "equalsraf/neovim-gui-shim",
     -- Indent level display plugin
-    "lukas-reineke/indent-blankline.nvim",
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("indent_blankline").setup {
+                show_end_of_line = true,
+                filetype_exclude = { "startify", "help", "qf", "vim-plug", "dashboard" },
+            }
+        end,
+    },
     -- Bookmark plugin.
-    "MattesGroeger/vim-bookmarks",
-    -- Start screen plugin.
-    "mhinz/vim-startify",
+    {
+        "MattesGroeger/vim-bookmarks",
+        config = function()
+            -- vim-bookmarks
+            vim.api.nvim_set_var("bookmark_sign", "⚑")
+            vim.api.nvim_set_var("bookmark_highlight_lines", 0)
+        end,
+    },
     -- gdb debugging plugin.
     { "sakhnik/nvim-gdb", run = "./install.sh" },
     -- Automatic pairing plug-in, which can automatically add or delete paired symbols, such as "()", "[]", "{}".
-    "windwp/nvim-autopairs",
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup {
+                map_cr = false,
+            }
+        end,
+    },
     -- Project configuration management plugin.
-    "aszswaz/project.nvim",
+    {
+        "aszswaz/project.nvim",
+        config = function()
+            require("project").setup {
+                autostart = "~/Documents/project/aszswaz",
+                shell = "/bin/bash",
+            }
+        end,
+    },
     -- Terminal plugin.
-    { "akinsho/toggleterm.nvim", tag = "*" },
+    {
+        "akinsho/toggleterm.nvim",
+        tag = "*",
+        config = function()
+            -- Terminal plugin.
+            require("toggleterm").setup {
+                hide_numbers = true,
+                shade_terminals = true,
+                -- The color depth of the terminal
+                shading_factor = "1",
+                start_in_insert = true,
+                insert_mappings = true,
+                terminal_mappings = true,
+                persist_szie = true,
+                -- Terminal window type
+                direction = "float",
+                close_on_exit = true,
+                -- Set the default shell.
+                shell = vim.o.shell,
+                -- Terminal's popup settings.
+                float_opts = {
+                    border = "rounded",
+                    width = math.floor(vim.o.columns / 5 * 4),
+                    height = math.floor(vim.o.lines / 5 * 4),
+                    -- transparency
+                    winblend = 0,
+                },
+                highlights = {
+                    NormalFloat = {
+                        guifg = "#DCDCDC",
+                        guibg = "#2C2C2C",
+                    },
+                    FloatBorder = {
+                        guifg = "#CCCCCC",
+                        guibg = "#2C2C2C",
+                    },
+                },
+                on_create = event.toggleterm_open,
+            }
+        end,
+    },
     -- Programming language helper plugins.
     { "neoclide/coc.nvim", branch = "release" },
     -- File manager.
@@ -39,6 +108,22 @@ local PLUGINS = {
         requires = {
             "kyazdani42/nvim-web-devicons",
         },
+        config = function()
+            require("nvim-tree").setup {
+                -- Enable git
+                git = {
+                    enable = true,
+                    ignore = false,
+                },
+                view = {
+                    mappings = {
+                        list = {
+                            { key = "<C-e>", action = "close" },
+                        },
+                    },
+                },
+            }
+        end,
     },
     -- Tab page plugin.
     {
@@ -64,6 +149,32 @@ local THEMES = {
     {
         "nvim-lualine/lualine.nvim",
         requires = { "kyazdani42/nvim-web-devicons" },
+        config = function()
+            -- Plugins that use buffer as tab.
+            require("bufferline").setup {
+                animation = true,
+                auto_hide = false,
+                tabpages = true,
+                closable = true,
+                clickable = true,
+                exclude_ft = { "qf", "fugitive" },
+                exclude_name = {},
+                icons = true,
+                icon_custom_colors = false,
+                icon_separator_active = "▎",
+                icon_separator_inactive = "▎",
+                icon_close_tab = "",
+                icon_close_tab_modified = "●",
+                icon_pinned = "車",
+                insert_at_end = true,
+                insert_at_start = false,
+                maximum_padding = 1,
+                maximum_length = 30,
+                semantic_letters = true,
+                letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
+                no_name_title = nil,
+            }
+        end,
     },
 }
 
