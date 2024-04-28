a41df854ad43726d1d2b2fe5e2c9d152046ec5de
aszswaz <aszswaz@163.com>
2024-04-28 23:10:22 +0800

    调整目录结构

--- test/init.lua
+++
@@ -1,6 +0,0 @@
-local PROJECT_DIR = vim.fn.expand("<script>:h:h")
-local runtimePaths = vim.api.nvim_list_runtime_paths()
-runtimePaths[1] = PROJECT_DIR
-vim.o.runtimepath = vim.fn.join(runtimePaths, ",")
-
-vim.cmd.runtime "init.lua"
--- test/logger.lua
+++
@@ -1,9 +0,0 @@
-local log = require "aszswaz.logger"
-
-function test01()
-    log.debug "debug\ndebugaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    log.info "info\ninfo"
-    log.warn "warn"
-    log.error "error"
-    log.showMessages()
-end
