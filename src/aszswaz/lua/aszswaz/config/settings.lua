93a3be020f5b57626c7925198e1aaf3df1adf330
aszswaz <aszswaz@163.com>
2024-04-29 21:59:12 +0800

    删除没用的 tags 文件

--- src/aszswaz/lua/aszswaz/config/settings.lua
+++ src/aszswaz/lua/aszswaz/config/settings.lua
@@ -26,7 +26,7 @@ local OPTIONS = {
     guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250,sm:block-blinkwait175-blinkoff150-blinkon175",
     guifont = "JetBrainsMono Nerd Font Mono:h12",
     formatexpr = 'v:lua.require("aszswaz.text").format(v:true)',
-    tags = "/usr/share/nvim/plugins/tags," .. stdpath("cache") .. "/tags"
+    tags = stdpath("cache") .. "/tags"
 }
 
 function M.setup()
