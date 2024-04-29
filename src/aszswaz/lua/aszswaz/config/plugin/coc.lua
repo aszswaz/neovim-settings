8cd24da53472989b66843421fc4a9f7d9abde97a
aszswaz <aszswaz@163.com>
2024-04-29 22:42:19 +0800

    backup

--- src/aszswaz/lua/aszswaz/config/plugin/coc.lua
+++ src/aszswaz/lua/aszswaz/config/plugin/coc.lua
@@ -1,22 +1,7 @@
 local M = {}
 
 local keyset = vim.api.nvim_set_keymap
-local get_current_line = vim.api.nvim_get_current_line
-local set_current_line = vim.api.nvim_set_current_line
-local get_current_win = vim.api.nvim_get_current_win
-local get_cursor = vim.api.nvim_win_get_cursor
 
-local visible = function()
-    return vim.fn["coc#pum#visible"]() == 1
-end
-local expandableOrJumpable = function()
-    return vim.fn["coc#expandableOrJumpable"]() == 1
-end
-local cancel = vim.fn["coc#pum#cancel"]
-local confirm = vim.fn["coc#pum#confirm"]
-local on_enter = vim.fn["coc#on_enter"]
-local rpc_request = vim.fn["coc#rpc#request"]
-local refresh = vim.fn["coc#refresh"]
 local coc_config = vim.fn["coc#config"]
 
 function M.setup()
@@ -27,29 +12,8 @@ end
 -- 注册快捷键
 function M.regHotKey()
     local options = { silent = true, unique = true, noremap = true, expr = true }
-    keyset("i", "<esc>", "", {
-        silent = options.silent,
-        unique = options.unique,
-        noremap = options.noremap,
-        expr = options.expr,
-        callback = M.escEvent,
-    })
-    keyset("i", "<CR>", "", {
-        silent = options.silent,
-        unique = options.unique,
-        noremap = options.noremap,
-        expr = options.expr,
-        callback = M.enterEvent,
-    })
-
-    keyset("i", "<C-space>", "", {
-        desc = "打开自动完成窗口，或者移动到下一个选项",
-        silent = options.silent,
-        unique = options.unique,
-        noremap = options.noremap,
-        expr = options.expr,
-        callback = M.autocomplete,
-    })
+    keyset("i", "<esc>", [[coc#pum#visible() ? coc#pum#cancel() : "\<esc>"]], options)
+    keyset("i", "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], options)
 end
 
 -- 配置 coc.nvim
@@ -78,34 +42,6 @@ function M.config()
             },
         },
     })
-end
-
--- 如果自动完成窗口已经打开，跳转到下一个选项，否则打开自动完成窗口
-function M.autocomplete()
-    if expandableOrJumpable() then
-        -- 跳转到下一个选项
-        rpc_request("doKeymap", { "snippets-expand-jump", "" })
-    else
-        refresh()
-    end
-end
-
--- 处理 esc 按键
-function M.escEvent()
-    if visible() then
-        cancel()
-    else
-        vim.cmd.stopinsert()
-    end
-end
-
--- 处理 enter 键
-function M.enterEvent()
-    if visible() then
-        confirm()
-    else
-        on_enter()
-    end
 end
 
 return M
